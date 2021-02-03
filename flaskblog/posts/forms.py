f96b52eeea56d7d4ee1054b62f674f3facba20f6
Kavitha Periyasamy <75384240+kavitha-candy@users.noreply.github.com>
2021-02-03 12:56:39 +0530

    Delete forms.py

--- flaskblog/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import data_required
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[data_required()])
-    Content = TextAreaField('Content', validators=[data_required()])
-    submit = SubmitField('Post')
